8f80bf122e0f17dc752e96fabf07182d69e28af1
TON6I8 <qaasimrehman175@gmail.com>
2025-03-31 19:39:30 +0100

    clean up problem 2 solution

--- problem_2.py
+++ problem_2.py
@@ -1,23 +1,10 @@
-# def fibonacci(num_terms):
-#     a, b = 0, 1
-#     for _ in range(num_terms):
-#         yield a
-#         a, b = b, a + b
-
-# # Print the first 10 Fibonacci numbers
-# for value in fibonacci(10):
-#     print(value)
-
 s = 0
 n = 4_000_000
 a = 0
 b = 1
 next = b  
-count = 1
 
 while next < n:
-    
-    count += 1
     a, b = b, next
     next = a + b
     
